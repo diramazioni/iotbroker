9a2e4f6c41223614988672c357ccbc5752b813f8
Eli Spizzichino <eli@diramazioni.net>
2023-07-10 11:47:46 +0200

    dashboard initial mockup

--- dash/app/layout.tsx
+++ dash/app/layout.tsx
@@ -3,6 +3,12 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+/*# start tw-elements 
+import "tw-elements/dist/css/tw-elements.min.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
+#end*/
 
 export const metadata: Metadata = {
   title: 'Create Next App',
