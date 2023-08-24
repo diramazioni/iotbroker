beecd41321a64889b87c701e3739ae59c70cee79
Eli Spizzichino <scrivi@gmail.com>
2023-08-24 16:50:44 +0200

    assets path

--- svelte-dash/svelte.config.js
+++ svelte-dash/svelte.config.js
@@ -14,8 +14,12 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		files: {
+			assets: dev ? '' : 'src/lib/assets' 
+		},
 		paths: {
-			base: dev ? '' : '/iot'
+			base: dev ? '' : '/iot',
+			assets: dev ? '' : '/home/gv/python3/iotbroker/svelte-dash/src/lib/assets'
 		},		
 	}
 };
