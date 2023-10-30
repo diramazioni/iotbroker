304f67689bc0204b6453f96bec780910c10fd813
Eli Spizzichino <scrivi@gmail.com>
2023-10-30 22:30:07 +0100

    revert device creation

--- message_parser.py
+++ message_parser.py
@@ -8,7 +8,7 @@ import logging
 
 from prisma import Json
 
-#from prisma.models import DateTime
+# from prisma.models import DateTime
 
 """
 Seed the DB with saved json messages from the MQTT broker
@@ -21,7 +21,6 @@ class MessageParser:
         self.db = Prisma()
         self.logger = logging.getLogger(__name__)
         self.units = {}
-        
 
     async def connect(self):
         logging.info("Connecting to DB")
@@ -35,15 +34,17 @@ class MessageParser:
         try:
             e = payload
             sens = {}
-            timestamp = datetime.utcfromtimestamp(int(e["timestamp"]/1000))  # .isoformat()
+            timestamp = datetime.utcfromtimestamp(
+                int(e["timestamp"] / 1000)
+            )  # .isoformat()
             logging.debug(f"Timestamp: {timestamp}")
-            
+
             # skip if an invalid timestamp
             if timestamp < datetime(2023, 1, 1):
                 logging.error("T" * 80)
                 logging.error(f"timestamp out of range {e['timestamp']}")
                 return
-            
+
             if "Camera" in e["id"] or "camera" in e["id"]:
                 device_type = "Camera"
             elif "ETRometer" in e["name"]:
@@ -56,15 +57,15 @@ class MessageParser:
                 device_type = "WeatherStation_s"
             logging.info("-" * 80)
             logging.info(device_type)
-            
+
             # Insert the full message in the db
             message = {
                 "timestamp": timestamp,
                 "device_type": device_type,
-                "message": Json(payload)
+                "message": Json(payload),
             }
             await self.db.messages.create(data=message)
-            
+
             # Camera message: insert directly ino the db
             if device_type == "Camera":
                 logging.info(f"Camera: {e['id']}")
@@ -79,7 +80,7 @@ class MessageParser:
                 for u in units_db:
                     self.units[(u.device_type, u.name)] = u.value
                 logging.debug(self.units)
-                
+
             # Create the units if they don't exist
             async def insert_units(device_type, unames={}):
                 for name, value in unames.items():
@@ -93,9 +94,10 @@ class MessageParser:
                         units_db = await self.db.units.create(data=units_obj)
                         logging.info(f"Create units {name}")
                         self.units[(device_type, name)] = value
-                        
+
                     else:
                         logging.debug("skipping " + name)
+
             # Capture all the properties
             if all(isinstance(item, (int, float)) for item in e["value"]):
                 sens[e["name"]] = dict(zip(e["controlledProperty"], e["value"]))
@@ -117,8 +119,11 @@ class MessageParser:
                 device_obj["areaServed"] = e["areaServed"]
             if "location" in e:
                 device_obj["location"] = str(e["location"]["coordinates"])
-            
-            # Create sensors 
+
+            # Create Device
+            device_db = await self.db.device.create(data=device_obj)
+
+            # Create sensors
             sens_obj = {
                 "device": {
                     "connect": {
@@ -127,15 +132,15 @@ class MessageParser:
                 },
                 "timestamp": timestamp,
             }
-            
+
             for sensor_name, d in sens.items():
                 logging.info(sensor_name)
                 if "SCD30" in sensor_name:  # ETRometer
                     sens_obj["name"] = sensor_name
-                    sens_obj.update(d)                    
+                    sens_obj.update(d)
                 else:  # Other devices
                     sens_obj.update(d)
-                    
+
             if device_type == "ETRometer":
                 await self.db.etrometer.create(data=sens_obj)
             elif device_type == "WeatherStation_v":
@@ -144,12 +149,9 @@ class MessageParser:
                 await self.db.weatherstation.create(data=sens_obj)
             elif device_type == "WeatherStation_s":
                 await self.db.weatherstationstd.create(data=sens_obj)
-                
+
             logging.info("-" * 80)
             logging.info(sens_obj)
-            
-            # Create Device
-            device_db = await self.db.device.create(data=device_obj)
 
         except Exception as e:
             logging.error(f"db_entry error:{e}")  # Print the exception
@@ -170,7 +172,8 @@ async def main() -> None:
     message_parser = MessageParser()
     await message_parser.connect()
     # await message_parser.process_data("data/ETRometer_2.json")
-    import glob 
+    import glob
+
     loop = asyncio.get_event_loop()
     background_tasks = set()
     for f in glob.glob("dev_data/test/*.json"):
