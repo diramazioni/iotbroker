6adfe0e6db19f5f132979c3248f54c9b9763e03d
Eli Spizzichino <scrivi@gmail.com>
2024-01-29 11:08:48 +0100

    add cert to credential

--- embedded/esp32-websocket-cam/src/esp32-websocket-cam.ino
+++ embedded/esp32-websocket-cam/src/esp32-websocket-cam.ino
@@ -19,47 +19,6 @@ String endOfStreamMessage = "END_OF_STREAM-" + deviceString;
 using namespace websockets;
 WebsocketsClient client;
 
-/*
-To get the SSL certificate run: 
-  
-    openssl s_client -showcerts -connect socketsbay.com:443
-
-See https://github.com/gilmaimon/ArduinoWebsockets/issues/101 for details.
-*/
-const char ssl_cert[]  = \
-
-"-----BEGIN CERTIFICATE-----\n"\
-"MIIFYDCCBEigAwIBAgIQQAF3ITfU6UK47naqPGQKtzANBgkqhkiG9w0BAQsFADA/\n"\
-"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n"\
-"DkRTVCBSb290IENBIFgzMB4XDTIxMDEyMDE5MTQwM1oXDTI0MDkzMDE4MTQwM1ow\n"\
-"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"\
-"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwggIiMA0GCSqGSIb3DQEB\n"\
-"AQUAA4ICDwAwggIKAoICAQCt6CRz9BQ385ueK1coHIe+3LffOJCMbjzmV6B493XC\n"\
-"ov71am72AE8o295ohmxEk7axY/0UEmu/H9LqMZshftEzPLpI9d1537O4/xLxIZpL\n"\
-"wYqGcWlKZmZsj348cL+tKSIG8+TA5oCu4kuPt5l+lAOf00eXfJlII1PoOK5PCm+D\n"\
-"LtFJV4yAdLbaL9A4jXsDcCEbdfIwPPqPrt3aY6vrFk/CjhFLfs8L6P+1dy70sntK\n"\
-"4EwSJQxwjQMpoOFTJOwT2e4ZvxCzSow/iaNhUd6shweU9GNx7C7ib1uYgeGJXDR5\n"\
-"bHbvO5BieebbpJovJsXQEOEO3tkQjhb7t/eo98flAgeYjzYIlefiN5YNNnWe+w5y\n"\
-"sR2bvAP5SQXYgd0FtCrWQemsAXaVCg/Y39W9Eh81LygXbNKYwagJZHduRze6zqxZ\n"\
-"Xmidf3LWicUGQSk+WT7dJvUkyRGnWqNMQB9GoZm1pzpRboY7nn1ypxIFeFntPlF4\n"\
-"FQsDj43QLwWyPntKHEtzBRL8xurgUBN8Q5N0s8p0544fAQjQMNRbcTa0B7rBMDBc\n"\
-"SLeCO5imfWCKoqMpgsy6vYMEG6KDA0Gh1gXxG8K28Kh8hjtGqEgqiNx2mna/H2ql\n"\
-"PRmP6zjzZN7IKw0KKP/32+IVQtQi0Cdd4Xn+GOdwiK1O5tmLOsbdJ1Fu/7xk9TND\n"\
-"TwIDAQABo4IBRjCCAUIwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYw\n"\
-"SwYIKwYBBQUHAQEEPzA9MDsGCCsGAQUFBzAChi9odHRwOi8vYXBwcy5pZGVudHJ1\n"\
-"c3QuY29tL3Jvb3RzL2RzdHJvb3RjYXgzLnA3YzAfBgNVHSMEGDAWgBTEp7Gkeyxx\n"\
-"+tvhS5B1/8QVYIWJEDBUBgNVHSAETTBLMAgGBmeBDAECATA/BgsrBgEEAYLfEwEB\n"\
-"ATAwMC4GCCsGAQUFBwIBFiJodHRwOi8vY3BzLnJvb3QteDEubGV0c2VuY3J5cHQu\n"\
-"b3JnMDwGA1UdHwQ1MDMwMaAvoC2GK2h0dHA6Ly9jcmwuaWRlbnRydXN0LmNvbS9E\n"\
-"U1RST09UQ0FYM0NSTC5jcmwwHQYDVR0OBBYEFHm0WeZ7tuXkAXOACIjIGlj26Ztu\n"\
-"MA0GCSqGSIb3DQEBCwUAA4IBAQAKcwBslm7/DlLQrt2M51oGrS+o44+/yQoDFVDC\n"\
-"5WxCu2+b9LRPwkSICHXM6webFGJueN7sJ7o5XPWioW5WlHAQU7G75K/QosMrAdSW\n"\
-"9MUgNTP52GE24HGNtLi1qoJFlcDyqSMo59ahy2cI2qBDLKobkx/J3vWraV0T9VuG\n"\
-"WCLKTVXkcGdtwlfFRjlBz4pYg1htmf5X6DYO8A4jqv2Il9DjXA6USbW1FzXSLr9O\n"\
-"he8Y4IWS6wY7bCkjCWDcRQJMEhg76fsO3txE+FiYruq9RUWhiF1myv4Q6W+CyBFC\n"\
-"Dfvp7OOGAN6dEOM4+qR9sdjoSYKEBpsr6GtPAQw4dy753ec5\n"\
-"-----END CERTIFICATE-----\n";
-
 
 
 extern TaskHandle_t loopTaskHandle;
@@ -74,7 +33,7 @@ void setup() {
   init_ws();
 
   //disableCore0WDT();
-  // WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  // WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout problems
   xTaskCreateUniversal(loopTask_Cmd, "loopTask_Cmd", 8192, NULL, 1, &loopTaskHandle, 0);		//loopTask_Cmd uses core 0.
   //xTaskCreateUniversal(loopTask_Blink, "loopTask_Blink", 8192, NULL, 1, &loopTaskHandle, 0);//loopTask_Blink uses core 0.
 }
@@ -92,27 +51,14 @@ void loopTask_Cmd(void *pvParameters) {
   while (1) {
     if (client.available()) {
       camera_fb_t * fb = NULL;
-      // camera_fb_t *fb = esp_camera_fb_get();
       fb = esp_camera_fb_get();
       if (fb != NULL) {
-        // Send binary frame header
-        // uint8_t header[4];
-        // header[0] = 0b10000000 | 0b00000010;  // FIN + Binary Opcode
-        // header[1] = fb->len < 126 ? fb->len : 126;  // Payload length (7 bits)
-        // header[2] = fb->len >= 126 ? (fb->len >> 8) & 0xFF : 0;  // Extended payload length (16 bits)
-        // header[3] = fb->len >= 126 ? fb->len & 0xFF : 0;  // Extended payload length (16 bits)
-
-        // // Send frame header
-        // client.sendBinary((const char*)header, sizeof(header));
-
         // Send binary data in chunks
         for (size_t i = 0; i < fb->len; i += 1024) {
           size_t chunkSize = std::min(static_cast<size_t>(1024), static_cast<size_t>(fb->len - i));          
           client.sendBinary((const char*)(fb->buf + i), chunkSize);
         }
-        // Signal the end of the stream as bytes
-        //client.sendBinary("END_OF_STREAM", 13);
-        // Send the combined message as text
+        // Send the the end of the stream as text
         client.send(endOfStreamMessage);
         esp_camera_fb_return(fb);
         Serial.println("MJPG sent");
