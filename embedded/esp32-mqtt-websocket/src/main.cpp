d538c053c7860e4eb3fd008433e9daeab10d1abe
Eli Spizzichino <scrivi@gmail.com>
2024-02-02 22:19:27 +0100

    esp32 MQTTPubSubClient mqtt on websocket

--- embedded/esp32-mqtt-websocket/src/main.cpp
+++ embedded/esp32-mqtt-websocket/src/main.cpp
@@ -1,59 +1,57 @@
 #include <Arduino.h>
-#include <WiFi.h>
+// #include <WiFi.h>
 #include <WiFiClientSecure.h>
-#include <PubSubClient.h>
+#include <WebSocketsClient.h> // include before MQTTPubSubClient.h
+#include <MQTTPubSubClient.h>
 
 #include "credentials.h"
 
+WebSocketsClient client;
+//WiFiClientSecure client;
+MQTTPubSubClient mqtt;
 
-WiFiClientSecure espClient;
-PubSubClient client(espClient);
+void callback(char* topic, byte* payload, unsigned int length) {
+  // Handle incoming MQTT messages here
+}
 
 void setup() {
   Serial.begin(9600);
 
   // Connect to Wi-Fi
+  
   WiFi.begin(ssid_Router, password_Router);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
-
-  // Set up MQTT server over WSS
-  espClient.setCACert(ssl_cert);
-  Serial.print("Connecting to NGINX server...");
-  if (espClient.connect(mqtt_server, mqtt_port)) {
-    Serial.println("espClient connected!");
+  //client.setCACert(ssl_cert);
+  client.beginSSL(mqtt_server, mqtt_port);
+  //client.connect(mqtt_server, mqtt_port);
+  // connect to mqtt broker
+  Serial.println("connect to mqtt broker");
+  mqtt.begin(client);
+  mqtt.connect("arduino");
+  if(mqtt.isConnected()) {
+      Serial.println("Connected to MQTT server");
+      // Subscribe to MQTT topics or perform other actions as needed
   } else {
-    Serial.println("Connection failed!");
-  }     
-  client.setServer(mqtt_server, mqtt_port);
+      Serial.print("MQTT Failed");
+  }
+  // subscribe callback which is called when every packet has come
+  mqtt.subscribe([](const String& topic, const String& payload, const size_t size) {
+      Serial.println("mqtt received: " + topic + " - " + payload);
+  });
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+  // if (!client.connected()) {
+  //   reconnect();
+  // }
+  // should be called to trigger callbacks
+  mqtt.update();
+  // publish message
+  mqtt.publish("/hello", "world");
+  delay(1000);
 }
 
-void reconnect() {
-  while (!client.connected()) {
- 
-    Serial.println("Attempting MQTT connection...");
-    if (client.connect("ESP32Client", mqtt_username, mqtt_password)) {
-      Serial.println("Connected to MQTT server");
-      // Subscribe to MQTT topics or perform other actions as needed
-    } else {
-      Serial.print("Failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" Retrying in 5 seconds");
-      delay(5000);
-    }
-  }
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  // Handle incoming MQTT messages here
-}
