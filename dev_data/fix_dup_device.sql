66cc5d34475f35e2e85269b003a958c2e0ed6469
Eli Spizzichino <scrivi@gmail.com>
2024-03-05 00:36:16 +0100

    fix duplicate device - sql speedup

--- dev_data/fix_dup_device.sql
+++ dev_data/fix_dup_device.sql
@@ -1,38 +1,31 @@
-UPDATE "WeatherStation"
-SET "deviceId" = new_primary_id
-FROM (
-    SELECT MIN(id) AS new_primary_id, name
-    FROM "Device"
-    GROUP BY name
-) AS new_primary
-WHERE "WeatherStation"."deviceId" IN (SELECT id FROM "Device" WHERE name = new_primary.name);
+-- UPDATE "WeatherStation"
+-- SET "deviceId" = new_primary_id
+-- FROM (
+--     SELECT MIN(id) AS new_primary_id, name
+--     FROM "Device"
+--     GROUP BY name
+-- ) AS new_primary
+-- WHERE "WeatherStation"."deviceId" IN (SELECT id FROM "Device" WHERE name = new_primary.name);
+CREATE TEMP TABLE temp_primary_mapping AS
+SELECT MIN(id) AS new_primary_id, name
+FROM "Device"
+GROUP BY name;
 
 UPDATE "WeatherStationStd"
-SET "deviceId" = new_primary_id
-FROM (
-    SELECT MIN(id) AS new_primary_id, name
-    FROM "Device"
-    GROUP BY name
-) AS new_primary
-WHERE "WeatherStationStd"."deviceId" IN (SELECT id FROM "Device" WHERE name = new_primary.name);
+SET "deviceId" =  temp.new_primary_id
+FROM temp_primary_mapping temp
+WHERE "WeatherStationStd"."deviceId" IN (SELECT id FROM "Device" WHERE name = temp.name);
 
 UPDATE "WeatherStationVirtual"
-SET "deviceId" = new_primary_id
-FROM (
-    SELECT MIN(id) AS new_primary_id, name
-    FROM "Device"
-    GROUP BY name
-) AS new_primary
-WHERE "WeatherStationVirtual"."deviceId" IN (SELECT id FROM "Device" WHERE name = new_primary.name);
+SET "deviceId" =  temp.new_primary_id
+FROM temp_primary_mapping temp
+WHERE "WeatherStationVirtual"."deviceId" IN (SELECT id FROM "Device" WHERE name = temp.name);
 
 UPDATE "ETRometer"
-SET "deviceId" = new_primary_id
-FROM (
-    SELECT MIN(id) AS new_primary_id, name
-    FROM "Device"
-    GROUP BY name
-) AS new_primary
-WHERE "ETRometer"."deviceId" IN (SELECT id FROM "Device" WHERE name = new_primary.name);
+SET "deviceId" =  temp.new_primary_id
+FROM temp_primary_mapping temp
+WHERE "ETRometer"."deviceId" IN (SELECT id FROM "Device" WHERE name = temp.name);
 
 DELETE FROM "Device"
 WHERE id NOT IN (SELECT MIN(id) FROM "Device" GROUP BY name);
+
